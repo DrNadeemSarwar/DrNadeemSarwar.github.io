3ea21cd6691e233247f3c74b604541231ce6c184
DrNadeemSarwar <155052864+DrNadeemSarwar@users.noreply.github.com>
2024-01-31 10:06:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-$(document).ready(function() {
-    // Get visitor information using ipinfo.io API
-    $.get("https://ipinfo.io", function(response) {
-        // Display visitor information in the footer
-        var visitorInfo = "Visitor from " + response.city + ", " + response.region + ", " + response.country;
-        $("#visitor-info").text(visitorInfo);
-    }, "jsonp");
-});
+function websiteVisits(response) {
+    document.querySelector("#visits").textContent = response.value;
+}
